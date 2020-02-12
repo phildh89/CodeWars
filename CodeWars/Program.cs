bfd50b22fec08e800827515804487f8012b874ee
phildh89 <phildh89@gmail.com>
2020-02-12 09:59:56 -0800

    Add project files.

---
+++ CodeWars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CodeWars.Program.Main()");
+
+        }
+
+    }
+}
