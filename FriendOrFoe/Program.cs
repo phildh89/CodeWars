951df5296a0989c26513fa0b5b6c94c0a57d3dac
phildh89 <phildh89@gmail.com>
2020-02-12 11:21:07 -0800

    Added FriendOrFoe Project

---
+++ FriendOrFoe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FriendOrFoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Ryan", "Kieran", "Mark", "Jimmy" };
+            Console.WriteLine(Kata.FriendOrFoe(names));
+        }
+
+    }
+    public static class Kata
+    {
+            
+            
+        public static IEnumerable<string> FriendOrFoe(string[] names)
+        {
+            List<string> listOfFriends = new List<string>();
+            foreach (var item in names)
+            {
+                if (item.Length == 4)
+                    listOfFriends.Add(item);
+            }
+            return listOfFriends;
+        }
+            
+    }
+}
