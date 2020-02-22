22a5c71b11e8caeb2ff2e38063e407a3cbea4172
phildh89 <phildh89@gmail.com>
2020-02-21 16:09:39 -0800

    Added new project SBMeme

---
+++ SBMeme/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SBMeme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SBMeme.Program.Main()");
+
+            Console.WriteLine(Meme("I love this class"));
+        }
+
+        public static string Meme(string inputPhrase)
+        {
+            string memeString = "";
+            for (int i = 0; i < inputPhrase.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    inputPhrase = inputPhrase.ToUpper();
+                    memeString += inputPhrase[i];
+                }
+                if (i % 2 == 0)
+                {
+                    inputPhrase = inputPhrase.ToLower();
+                    memeString += inputPhrase[i];
+                }
+            }
+            return memeString;
+        }
+    }
+}
