c75c7198ed93670c3d9a27d898294e6b404f6ca7
phildh89 <phildh89@gmail.com>
2020-03-02 14:11:28 -0800

    Add Coin Program

---
+++ Coins/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coins.Program.Main()");
+
+            CoinCount(121.37);
+        }
+        public static void CoinCount(double input)
+        {
+            int centAmount = Convert.ToInt32(input*100);
+            int quarters = centAmount / 25;
+            centAmount = centAmount % 25;
+
+            int dimes = centAmount / 10;
+            centAmount = centAmount % 10;
+
+            int nickles = centAmount / 5;
+            int pennies = centAmount % 5;
+
+             
+
+            Console.WriteLine($"Quarters: {quarters}\nDimes: {dimes}\nNickles:{nickles}\nPennies:{pennies}");
+
+        }
+    }
+}
