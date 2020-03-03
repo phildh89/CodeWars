fc50c8e8dbe62cd66e467c6e0fa4553f8b60314b
phildh89 <phildh89@gmail.com>
2020-03-03 15:45:53 -0800

    Add CopyArray Project

---
+++ CopyArray/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CopyArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CopyArray.Program.Main()");
+            CopyArray();
+        }
+
+        public static void CopyArray()
+        {
+            Console.Write("How many numbers are in your array: ");
+
+            int indexNum = Convert.ToInt32(Console.ReadLine());
+            int[] input = new int[indexNum];
+            
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine($"Enter number {i+1}: ");
+                input[i] += Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (var k in input)
+            {
+                Console.Write(k);
+            }
+        }
+    }
+}
