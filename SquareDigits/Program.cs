f3a18cbaa6574a919150ec1fbaf7785152088225
phildh89 <phildh89@gmail.com>
2020-03-16 11:35:35 -0700

    Added SquareDigits

---
+++ SquareDigits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SquareDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SquareDigits.Program.Main()");
+            Console.WriteLine(SquareDigits(9119));
+        }
+        public static int SquareDigits(int n)
+        {
+            string output = "";
+            foreach (char c in n.ToString())
+            {
+                int square = int.Parse(c.ToString());
+                output += (square * square);
+            }
+            return int.Parse(output);
+        }
+    }
+}
