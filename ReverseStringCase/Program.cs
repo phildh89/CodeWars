252030c043da8e82c6e43466187e6260f5b44cac
Philip Ho <phildh89@gmail.com>
2020-03-24 18:05:09 -0700

    Added ReverseStringCase

---
+++ ReverseStringCase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ReverseStringCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseCase();
+        }
+        public static void ReverseCase() 
+        {
+            Console.WriteLine("Enter a string:");
+            string input = Convert.ToString(Console.ReadLine());
+            string lowerInput = input.ToLower();
+            string upperInput = input.ToUpper();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (lowerInput[i] == input[i])
+                {
+                    Console.Write(upperInput[i]);
+                    continue;
+                }
+                Console.Write(lowerInput[i]);
+            }
+            
+        }
+    }
+}
