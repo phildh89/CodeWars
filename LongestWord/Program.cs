44c8d5570349ac934d0f52f29f194f444624b324
Philip Ho <phildh89@gmail.com>
2020-03-31 16:00:01 -0700

    Updated LongestWord

---
+++ LongestWord/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LongestWord("the quick brown fox");
+            Test1("the quick brown fox");
+        }
+
+        public static void LongestWord(string input)
+        {
+            string[] inputSplit = input.Split(" ");
+            int count = 0;
+
+            for (int i = 0; i < inputSplit.Length; i++)
+            {
+
+                if (inputSplit[i].Length >= count)
+                {
+                    count = inputSplit[i].Length;
+                }
+            }
+            for (int i = 0; i < inputSplit.Length; i++)
+            {
+
+                if (inputSplit[i].Length == count)
+                {
+                    Console.WriteLine(inputSplit[i]);
+                }
+            }
+
+        }
+
+        public static void Test1(string input)
+        {
+            string[] words = input.Split(' ');
+            string largestword = "";
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > largestword.Length)
+                {
+                    largestword = words[i];
+                }
+            }
+            Console.Write($"The largest word is {largestword}.");
+        }
+    }
+}
