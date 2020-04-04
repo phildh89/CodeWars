14516fff7be161068449a1a1d001e26a25ba1dc8
Philip Ho <phildh89@gmail.com>
2020-04-04 14:19:40 -0700

    Add CountBits

---
+++ CountBits/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CountBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountBits(1234));
+        }
+        public static int CountBits(int input)
+        {
+            string bit = "";
+            int count = 0;
+            int binCount = 0;
+            for (int i = 0; Math.Pow(2, i) <= input; i++)
+            {
+                binCount++;
+            }
+
+            int exponent = binCount - 1;
+            for (int i = binCount; i > 0; i--)
+            {
+                if (Math.Pow(2, exponent) <= input)
+                {
+                    input -= Convert.ToInt32(Math.Pow(2, exponent));
+                    bit += 1;
+                    exponent--;
+                    count++;
+                    continue;
+                }
+                bit += 0;
+                exponent--;
+            }
+
+            return count;
+        }
+    }
+}
