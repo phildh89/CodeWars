17ca805d65e29729b1a831c4a7ae965b7b32ec06
Philip Ho <phildh89@gmail.com>
2020-04-28 14:50:33 -0700

    Added DigitialRoot

---
+++ DigitialRoot/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DigitialRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DigitRoot(35165132));
+        }
+        public static int DigitRoot(long n)
+        {
+            Console.WriteLine(n);
+            long result = 0;
+            List<long> digitList = new List<long>(); 
+
+            for (int i = 0; n != 0; i++)
+            {
+                long temp = (n % 10);
+                n /= 10;
+                digitList.Add(temp);
+            }
+            foreach (var x in digitList)
+            {
+                result += x;
+            }
+            if (result > 9)
+            {
+                result = DigitRoot(result);
+
+            }
+ 
+            return (int)result;
+            
+        }
+    }
+}
