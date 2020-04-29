c9cc8968719aa00aa0ad99b559b37dec688eb93b
Philip Ho <phildh89@gmail.com>
2020-04-28 22:51:05 -0700

    Added Accumulate

---
+++ Accumul/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Accumul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Accum("Hello World!"));
+        }
+        public static string Accum(string s)
+        {
+            string result = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += char.ToUpper(s[i]);
+                for (int k = i; k > 0; k--)
+                {
+                    result += char.ToLower(s[i]);
+                }
+                if (i != s.Length - 1)
+                {
+                    result += "-";
+                }
+
+            }
+            return result;
+        }
+    }
+}
